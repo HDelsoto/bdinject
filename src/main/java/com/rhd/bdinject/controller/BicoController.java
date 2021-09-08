68cd791bed53531e4a05cb5ac4a3f0f551fd1899
HDelsoto <higordel@hotmail.com>
2021-09-08 20:01:58 -0300

    Controller Bico / novos endpoints

--- src/main/java/com/rhd/bdinject/controller/BicoController.java
+++ src/main/java/com/rhd/bdinject/controller/BicoController.java
@@ -3,11 +3,12 @@ package com.rhd.bdinject.controller;
 
 import com.rhd.bdinject.model.Bico;
 import com.rhd.bdinject.repository.BicoRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -16,8 +17,44 @@ public class BicoController {
     @Autowired
     private BicoRepository repository;
 
+    @PostMapping
+    public Bico adicionar(@Valid @RequestBody Bico bico) {
+        return repository.save(bico);
+    }
+
     @GetMapping
     public List<Bico> listar() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Bico> buscar(@PathVariable Long id) {
+        Bico bico = repository.getOne(id);
+        if (bico == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(bico);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Bico> atualizar(@PathVariable Long id,
+                                             @Valid @RequestBody Bico bico) {
+        Bico existente = repository.getOne(id);
+        if (existente == null) {
+            return ResponseEntity.notFound().build();
+        }
+        BeanUtils.copyProperties(bico, existente, "id");
+        existente = repository.save(existente);
+        return ResponseEntity.ok(existente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable Long id) {
+        Bico bico = repository.getOne(id);
+        if (bico == null) {
+            return ResponseEntity.notFound().build();
+        }
+        repository.delete(bico);
+        return ResponseEntity.noContent().build();
     }
 }
