3ab15a3790010f321952ea49874c458d10468185
HDelsoto <higordel@hotmail.com>
2021-09-09 19:58:40 -0300

    Controller Bico / findNomeById 2

--- src/main/java/com/rhd/bdinject/repository/BicoRepository.java
+++ src/main/java/com/rhd/bdinject/repository/BicoRepository.java
@@ -1,5 +1,4 @@
 package com.rhd.bdinject.repository;
-
 import com.rhd.bdinject.model.Bico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,8 +6,8 @@ import org.springframework.data.repository.query.Param;
 
 public interface BicoRepository extends JpaRepository<Bico, Long> {
 
-    @Query("SELECT b.nome FROM Bico b where b.id = :id")
-    String findNomeById(@Param("id") Long id);
+    /*@Query("SELECT b.nome FROM Bico b where b.id = :id")
+    String findNomeById(@Param("id") Long id);*/
 
 }
 
